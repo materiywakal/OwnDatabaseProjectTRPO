8efb47e4ce12d1f6cb76ba2618f8a44ce733e223
Nikita Posled <www.nikitka09@gmail.com>
2018-03-01 16:28:25 +0300

    First commit with model

---
+++ Database/DB/Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBModel
+{
+    class Methods
+    {
+        string undefSymbols = "#^&()-=+[]~'//\\.,;|?";
+        public bool isThereNoUndefinedSymbols(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (undefSymbols.Contains(str[i])) return false;
+            }
+            return true;    
+        }
+    }
+}
---
+++ Database/DB/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DBModel
+{
+    public struct TableProperty
+    {
+        public TableProperty(string name, Type dataType, bool allowsnull, bool isFkey, bool isPkey, object def)
+        {
+            Name = name;
+            DataType = dataType;
+            AllowsNull = allowsnull;
+            Default = default(object);
+            if (def != null)
+            {
+                Type buf = def.GetType();
+                if (buf.GetType() == dataType) Default = def;
+            }
+            dataList = new List<object>();
+           
+            isForeignKey = isFkey;
+            isPrimaryKey = isPkey;
+            if (isPrimaryKey)
+            {
+                Type[] itf = DataType.GetInterfaces();
+                foreach (Type i in itf)
+                {
+                    Console.WriteLine("Class {0} implements {1}", DataType.Name, i.Name);
+                }
+            }
+        }
+        string Name;
+        public Type DataType;
+        bool AllowsNull;
+        object Default;
+        bool isPrimaryKey;
+        bool isForeignKey;
+
+        List<object> dataList;
+    }
+    class Table
+    {
+        List<TableProperty> Tables;
+        string Name;
+        public Table(string name)
+        {
+            Tables = new List<TableProperty>();
+            Name = name;
+        }
+        public void AddTable(TableProperty newTable)
+        {
+            Tables.Add(newTable);
+        }
+    }
+    class DB
+    {
+        List<TableProperty> TablesDB;
+        
+        public void AddElement(object[] arguments)
+        {
+            int TablesCount = TablesDB.Count;
+
+        }
+        public string kek;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TableProperty pp = new TableProperty("pp", typeof(string), false, false, true, null);
+            List<DB> k = new List<DB>();
+            for (int i = 0; i < 10; i++)
+            {
+                k.Add(new DB { kek = i.ToString() });
+
+            }
+            foreach (DB db in k)
+            {
+                db.kek = "lul";
+            }
+
+        }
+    }
+}
