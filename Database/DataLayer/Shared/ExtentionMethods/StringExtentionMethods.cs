9541776255a9bc3ce3e09b25c514c514760baa1d
R1kNk <www.nikitka09@gmail.com>
2018-03-03 10:06:56 +0300

    deleted some //)0 and updated method for undefined symbols

--- Database/DataLayer/Shared/ExtentionMethods/StringExtentionMethods.cs
+++ Database/DataLayer/Shared/ExtentionMethods/StringExtentionMethods.cs
@@ -8,18 +8,15 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace DataLayer.Shared.ExtentionMethods
 {
-    //Methods name is meanless
     public static class ExtensionMethods
     {
         static string undefSymbols = "#^&()-=+[]~'//\\.,;|? ";
 
-        // It's should be rewrited as an extention method, to avoid
-        // directly call
        static public bool isThereNoUndefinedSymbols(this string str)
         {
-            for (int i = 0; i < str.Length; i++)
+            foreach(char stringSymbol in str)
             {
-                if (undefSymbols.Contains(str[i])) return false;
+                if (undefSymbols.Contains(stringSymbol)) return false;
             }
             return true;    
         }
