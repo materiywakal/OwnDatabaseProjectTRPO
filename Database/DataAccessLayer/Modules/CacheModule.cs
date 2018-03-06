227e48845706e16dfb15f202bf94ee4bcc369cb1
Nikita Posled <www.nikitka09@gmail.com>
2018-03-06 18:02:40 +0300

    Add dependence to  AccessLayer

--- Database/DataAccessLayer/Modules/CacheModule.cs
+++ Database/DataAccessLayer/Modules/CacheModule.cs
@@ -8,6 +8,7 @@ namespace DataAccessLayer.Modules
 {
     class CacheModule
     {
+       
         // this is save to file module
         // and also here should be implemented startup method
         // for kernel instance initialize
