cb2cfe6cfe0514f63482c4f1e911aef6f50b317c
Klaus <nikolya.sidorenko.1999@mail.ru>
2018-03-07 16:53:04 +0300

    add the function of creating database

--- Database/UILayer/Interpretator.cs
+++ Database/UILayer/Interpretator.cs
@@ -8,5 +8,71 @@ namespace UILayer
 {
     class Interpretator
     {
+        static List<string> MainKeywords = new List<string>()
+        {
+            "CREATE_DATABASE",
+            "CREATE_TABLE"
+        };
+
+        public static void Run()
+        {
+            while(true)
+            {
+                string query = default(string);
+                query = Console.ReadLine();
+                char[] seprator = new char[] { ' ' };
+                string[] queryList = query.Split(seprator, 2);
+                string keyWord = queryList[0].ToUpper();
+
+                switch (keyWord)
+                {
+                    case "CREATE_DATABASE":
+                        {
+                            CreateDatabase(query);
+                        }break;
+
+                    case "CREATE_TABLE":
+                        {
+
+                        }break;
+
+                    default:
+                        Console.WriteLine($"\nERROR: Command '{keyWord}' doesn't found\n");
+                        break;
+                }
+
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+        private static void CreateDatabase(string query)
+        {
+            char[] separators = new char[] { ' ', ';' };
+            string[] queryList = query.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            if(queryList.Length==2)
+            {
+                if(queryList[1].ToUpper() != queryList[0].ToUpper())
+                {
+                    Console.WriteLine($"Database was created with name '{queryList[1]}'\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nERROR: Name of the database can't be a keyword\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"\nERROR: Invalid numbers of variables\n");
+            }
+        }
     }
 }
--- Database/UILayer/Program.cs
+++ Database/UILayer/Program.cs
@@ -12,12 +12,7 @@ namespace UILayer
     {
         static void Main(string[] args)
         {
-            //Only for testing purposes!!
-            var some = Kernel.GetInstance("first");
-            some.AddTable(new Table("same"));
-            string xx = default(string);
-            xx = Console.ReadLine();
-           
+            Interpretator.Run();
         }
     }
 }
