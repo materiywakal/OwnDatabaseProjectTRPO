e0621df58615a00707aa796d874a825411c3e256
Klaus <nikolya.sidorenko.1999@mail.ru>
2018-03-09 14:22:16 +0300

    fix some troubles

--- Database/UILayer/Interpretator.cs
+++ Database/UILayer/Interpretator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.InternalDataBaseInstanceComponents;
 
 namespace UILayer
 {
@@ -11,51 +13,77 @@ namespace UILayer
         static List<string> MainKeywords = new List<string>()
         {
             "CREATE_DATABASE",
-            "CREATE_TABLE"
+            "CREATE_TABLE",
+            "INFO",
+            "ALL_DATABASES",
+
         };
 
         public static void Run()
         {
-            while(true)
+            while (true)
             {
                 string query = default(string);
                 query = Console.ReadLine();
-                char[] seprator = new char[] { ' ' };
-                string[] queryList = query.Split(seprator, 2);
-                string keyWord = queryList[0].ToUpper();
+                string keyWord = GetMainKeyword(query);
 
                 switch (keyWord)
                 {
                     case "CREATE_DATABASE":
                         {
                             CreateDatabase(query);
-                        }break;
-
+                        }
+                        break;
                     case "CREATE_TABLE":
+                        {
+                            CreateTable(query);
+                        }
+                        break;
+                    case "INFO":
                         {
 
                         }break;
+                    case "ALL_DATABASES":
+                        {
 
+                        }break;
                     default:
                         Console.WriteLine($"\nERROR: Command '{keyWord}' doesn't found\n");
                         break;
                 }
             }
-
-
-
-
         }
+        #region LocalMethods
+        private static string GetMainKeyword(string query)
+        {
+            char[] seprator = new char[] { ' ' };
+            string[] queryList = query.Split(seprator, 2, StringSplitOptions.RemoveEmptyEntries);
+            return queryList[0].ToUpper();
+        }
+
+        private static bool IsKeyword(string name)
+        {
+            name = name.ToUpper();
+            foreach (var key in MainKeywords)
+                if (key == name)
+                    return true;
+            return false;
+        }
+
+        #endregion
+
+        #region MainMetods
 
         private static void CreateDatabase(string query)
         {
             char[] separators = new char[] { ' ', ';' };
             string[] queryList = query.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
-            if(queryList.Length==2)
+            if (queryList.Length == 2)
             {
-                if(queryList[1].ToUpper() != queryList[0].ToUpper())
+                if (IsKeyword(queryList[1]))
                 {
+                    Kernel.AddDBInstance(queryList[0]);
                     Console.WriteLine($"Database was created with name '{queryList[1]}'\n");
                 }
                 else
@@ -68,5 +96,15 @@ namespace UILayer
                 Console.WriteLine($"\nERROR: Invalid numbers of variables\n");
             }
         }
+
+        private static void CreateTable(string query)
+        {
+            
+        }
+
+
+
+        #endregion
+
     }
 }
