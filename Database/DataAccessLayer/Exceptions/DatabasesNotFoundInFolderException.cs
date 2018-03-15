3a560b259801b94ee5b50175d4aa749b2f6b95a0
Uladzimir Masla <uladzimir.masla@itechart-group.com>
2018-03-15 20:58:03 +0300

    Fix Esception access modifier in DataAccessLayer

--- Database/DataAccessLayer/Exceptions/DatabasesNotFoundInFolderException.cs
+++ Database/DataAccessLayer/Exceptions/DatabasesNotFoundInFolderException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Exceptions
 {
-    class DatabasesNotFoundInFolderException : Exception
+    public class DatabasesNotFoundInFolderException : Exception
     {
         public DatabasesNotFoundInFolderException()
         { }
