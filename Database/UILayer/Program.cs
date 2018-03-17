4d9c8f6d1197d2be73fd6d8ad2f826a055104292
Klaus <nikolya.sidorenko.1999@mail.ru>
2018-03-17 13:37:59 +0300

    add prototype of encrypt

--- Database/UILayer/Program.cs
+++ Database/UILayer/Program.cs
@@ -12,23 +12,12 @@ namespace UILayer
     {
         static void Main(string[] args)
         {
-            // test
-            //Kernel.GetInstance().Add(new DataBaseInstance("testDB"));
-            //Kernel.GetInstance()[0].AddTable("tableTest");
-            //Column buf = new Column("testColumn", typeof(string), true, false, false, "kk");
-            //Kernel.GetInstance()[0].TablesDB[0].AddColumn(buf);
-            //Kernel.GetInstance()[0].TablesDB[0].AddTableElement(new object[1] { null });
-            //Kernel.GetInstance().Add(new DataBaseInstance("testDB2"));
-            //Kernel.GetInstance()[1].AddTable("tableTest1");
-            //Column buf1 = new Column("testColumn2", typeof(int), true, false, false, 99);
-            //Kernel.GetInstance()[1].TablesDB[0].AddColumn(buf1);
-            //Kernel.GetInstance()[1].TablesDB[0].AddTableElement(new object[1] { null });
-            //Kernel.SaveAllDatabases();
-            //Kernel.OutNamesOfExistingDBs();
-            //
-            //Interpretator.Run();
-
-            Console.WriteLine(Convert.ToBoolean("xui"));
+            Kernel.AddDBInstance("dbTest");
+            var inst = Kernel.GetInstance("dbTest");
+            inst.AddTable("table1");
+            inst.SaveDataBaseInstanceToFolder();
+            Kernel.OutDatabaseInfo();
+            ////Interpreter.Run();
         }
     }
 }
